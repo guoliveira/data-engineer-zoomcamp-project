45c41fb33dc9694d66e7a3c569723f1d224eb4cc
Luis Guilherme Sousa de Oliveira <lgsoliveira@gmail.com>
2022-04-19 16:07:03 +0100

    Update create_view_visio.sql

--- SQL/create_view_visio.sql
+++ SQL/create_view_visio.sql
@@ -1,12 +1,15 @@
   CREATE VIEW `capstone-luis-oliveira-347008.weather_historical_data.weather_data_visio` AS
     SELECT
       wd.date
-      , wd.TAVG as temp_avg
       , wd.year
+      ,substr(wd.date, 5 ,2) as month
+      , wd.TAVG as temp_avg
       , ws.lat
       , ws.long
       , if(ws.Region='Continente', concat(ws.Area,' - ',ws.Region),ws.Region ) as portuguese_region
+      ,concat(ws.lat,' , ',ws.long) as geo
     FROM
       `capstone-luis-oliveira-347008.weather_historical_data.weather_data` wd
     INNER JOIN `capstone-luis-oliveira-347008.weather_historical_data.weather_stations` ws
-    on wd.stations_code=ws.code
+    on wd.stations_code=ws.code;
+
